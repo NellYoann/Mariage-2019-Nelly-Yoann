841d978269f5bee941b7f0ae046681bef9196a63
Clement <nelly.yoann.brimalho@gmail.com>
2019-04-24 23:27:43 +0200

    Update intro

--- dist/js/app.js
+++ dist/js/app.js
@@ -10400,8 +10400,10 @@ $(document).ready(function() {
 		var intro = new TimelineMax();
 
 		intro.delay(.5);
-		intro.from('.banner .banner-content', 1, {alpha: 0, ease: Power2.easeOut});
-		intro.from('.banner .banner-back', 1, {alpha: 0, top: '+=50', ease: Power2.easeOut}, '-=.75');
+		// intro.from('.banner .banner-content', 1, {alpha: 0, ease: Power2.easeOut});
+		intro.from('.banner .banner-back', 1, {alpha: 0, top: '+=50', ease: Power2.easeOut});
+		intro.staggerFrom('.banner .banner-content *', 1, {alpha: 0, ease: Power2.easeOut}, .25);
+		// intro.from('.banner .banner-back', 1, {alpha: 0, top: '+=50', ease: Power2.easeOut}, '-=.75');
 
 		intro.play();
 	}
